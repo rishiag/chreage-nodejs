4ce31ba42593fbe63365e3fcafbd0e8b4ad81d49
rishiag <rishiag.iitd@gmail.com>
2013-12-22 18:37:46 +0000

    heroku

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+var http = require('http');
+var fs  = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
 
-app.use(logfmt.requestLogger());
+function send404(response) {
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  response.write('Error 404: resource not found.');
+  response.end();
+}
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+function sendFile(response, filePath, fileContents) {
+  response.writeHead(
+    200, 
+    {"content-type": mime.lookup(path.basename(filePath))}
+  );
+  response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath) {
+  if (cache[absPath]) {
+    sendFile(response, absPath, cache[absPath]);
+  } else {
+    fs.exists(absPath, function(exists) {
+      if (exists) {
+        fs.readFile(absPath, function(err, data) {
+          if (err) {
+            send404(response);
+          } else {
+            cache[absPath] = data;
+            sendFile(response, absPath, data);
+          }
+        });
+      } else {
+        send404(response);
+      }
+    });
+  }
+}
+
+var server = http.createServer(function(request, response) {
+  var filePath = false;
+
+  if (request.url == '/') {
+    filePath = 'public/index.html';
+  } else {
+    filePath = 'public' + request.url;
+  }
+
+  var absPath = './' + filePath;
+  serveStatic(response, cache, absPath);
 });
 
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+server.listen(8080, function() {
+  console.log("Server listening on port 8080.");
 });
 
